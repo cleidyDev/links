3bd1b2971e2193d9f44594a9e81268e220a76fb3
Cleidy Fumanca <cleidydev@gmail.com>
2026-02-23 21:26:21 +0100

    feat:config hearder in home page

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,4 +1,19 @@
 import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
-    
+    header:{
+        paddingHorizontal:24,
+        width:"100%",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        marginBottom:32,
+    },
+    container:{
+        paddingTop:62,
+        flex:1
+    },
+    logo:{
+        height:32,
+        width: 38,
+    }
 })
