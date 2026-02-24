af2aa36e64771ccb4074cd89b637cd2510f43ac9
Cleidy Fumanca <cleidydev@gmail.com>
2026-02-24 21:24:48 +0100

    feat: add category componente

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,15 +2,19 @@ import {Image,View,TouchableOpacity} from 'react-native'
 import {MaterialIcons} from "@expo/vector-icons"
 import {styles} from "./styles"
 import { colors } from '@/styles/color'
+import { Category} from "@/components/category"
+
 export default function Index(){
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
                 <TouchableOpacity activeOpacity={0.8}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+             <Category/>
         </View>
     )
 }
---
+++ src/components/category/index.tsx
@@ -0,0 +1,15 @@
+import { colors } from "@/styles/color";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Pressable,Text } from "react-native";
+import { styles } from "./styles";
+
+export function Category(){
+    return(
+        <Pressable style={styles.container}>
+            <MaterialIcons name="code" size={16} color={colors.gray[400]} />
+            <Text style={styles.name}>
+                Projectos
+            </Text>
+        </Pressable>
+    )
+}
